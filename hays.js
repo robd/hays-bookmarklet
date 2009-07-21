d21f9a84b82c59e60bd2fdb563bfa822a23ded87
robd <github@robd.co.uk>
2009-07-22 00:22:49 +0100

    First Version

---
+++ hays.js
@@ -0,0 +1,29 @@
+function getElement(elementName) { 
+        return window.frames[2].document.getElementsByName(elementName)[0]; 
+}; 
+function selectDropdownIndex(elementName, index) { 
+        getElement(elementName).selectedIndex=index 
+}; 
+function markRangeAsAbsence(startDay,endDay,absenceType) { 
+        for (var dayOfWeek=startDay;dayOfWeek<=endDay;dayOfWeek=dayOfWeek+1) { 
+                selectDropdownIndex("absencetype"+dayOfWeek,absenceType); 
+                selectDropdownIndex("absenceunit"+dayOfWeek,1); 
+        } 
+}; 
+function markRangeAsWorked(startDay,endDay) { 
+        for (var dayOfWeek=startDay;dayOfWeek<=endDay;dayOfWeek=dayOfWeek+1) { 
+                getElement("hours"+dayOfWeek).value=1; selectDropdownIndex("hoursdd"+dayOfWeek,1); 
+        } 
+}; 
+function fillInNormalWeek() { 
+        markRangeAsAbsence(1,2,3); 
+        markRangeAsWorked(3,7); 
+        selectDropdownIndex("Invoice",1); 
+        getElement("NewRate").focus(); 
+}; 
+function fillInHolidayWeek() { 
+        markRangeAsAbsence(1,7,3); 
+        selectDropdownIndex("Invoice",1); 
+        getElement("NewRate").focus(); 
+}; 
+fillInNormalWeek();
