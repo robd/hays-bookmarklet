0894f13e0f723a041ecb268705814c74d910c36f
robd <github@robd.co.uk>
2010-03-28 14:58:56 +0100

    support marking days as absence by clicking bookmarklet twice

--- hays.js
+++ hays.js
@@ -11,13 +11,25 @@
 			selectDropdownIndex("absenceunit"+dayOfWeek,1);
 		}
 	};
-	function markRangeAsWorked(startDay,endDay) {
-		for (var dayOfWeek=startDay;dayOfWeek<=endDay;dayOfWeek=dayOfWeek+1) {
+	function markWeekdaysAsWorked() {
+		for (var dayOfWeek=3;dayOfWeek<=7;dayOfWeek=dayOfWeek+1) {
 			getElement("hours"+dayOfWeek).value=1;
 			selectDropdownIndex("hoursdd"+dayOfWeek,1);
 		}
 	};
+	function weekdaysMarkedAsWorked() {
+		for (var dayOfWeek=3;dayOfWeek<=7;dayOfWeek=dayOfWeek+1) {
+			if(getElement("hours"+dayOfWeek).value == "") {
+				return false;
+			}
+		}
+		return true;
+	};
 	markDaysAsAbsence(2);
-	markRangeAsWorked(3,7);
+	if (weekdaysMarkedAsWorked()) {
+		markDaysAsAbsence(7);
+	} else 
+		markWeekdaysAsWorked();
+	}
 	getElement("Password").focus();
 })();
